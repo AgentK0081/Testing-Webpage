a76b3028b24f9140fd50d583c07539049376291a
AgentK0081 <harryjoji08@gmail.com>
2025-12-27 14:33:16 +0530

    Add players array with member details

---
+++ members.js
@@ -0,0 +1,20 @@
+const players = [
+  {
+    username: "AgentK_0081",
+    callsign: "FED001",
+    rank: "Director",
+    suspended: false
+  },
+  {
+    username: "MinPRPO08",
+    callsign: "FED014",
+    rank: "Special Agent",
+    suspended: false
+  },
+  {
+    username: "crnac",
+    callsign: "FED022",
+    rank: "Probationary Special Agent",
+    suspended: true
+  }
+];
